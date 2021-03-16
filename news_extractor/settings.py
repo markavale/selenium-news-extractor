e9c6ff576eb2fba1715b9b0a8a561155f301950e
Mark Anthony Vale <markavale1@gmail.com>
2021-03-16 18:46:07 +0800

    update git ignore

--- news_extractor/settings.py
+++ news_extractor/settings.py
@@ -28,7 +28,7 @@ USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Ge
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
-DEBUG = False
+LOG_ENABLED = False
 
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
